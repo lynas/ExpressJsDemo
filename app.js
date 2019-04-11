7c2f9cd52ba1c30bbf323cb4366f4239253b903f
sazzad <sazzad@hsenidmobile.com>
2019-04-11 10:48:58 +0530

    router/controller added

--- app.js
+++ app.js
@@ -3,16 +3,7 @@ const app = express();
 const logger = require('./middleware/logger');
 app.use(logger);
 
-const isuser = {
-    id:1,
-    name:"name"
-};
 
-app.get('/api/user', (req, resp) => {
-    resp.json(isuser);
-});
-
-
-
+app.use('/api/members', require('./routes/api/members'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running at : http://localhost:${port}`));
---
+++ routes/api/members.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const members = [
+    {
+        id: 1,
+        name: "name1"
+    },
+    {
+        id: 2,
+        name: "name2"
+    }
+];
+
+router.get('/', (req, resp) => {
+    resp.json(members);
+});
+
+
+router.get('/:id', (req, resp) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (!found) {
+        resp.status(404).json({msg: `Member not found with ID : ${req.params.id}`});
+    }
+    resp.json(members.filter(member => member.id === parseInt(req.params.id)));
+
+});
+
+module.exports = router;
